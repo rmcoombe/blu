d612f804ac0bc009e24bfa8771530c854f303635
rmcoombe <rosscoombe@gmail.com>
2020-09-10 19:51:32 +0100

    fghngf
    fnf

--- www/js/index.js
+++ www/js/index.js
@@ -1,89 +1,105 @@
+// (c) 2014 Don Coleman
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/* global mainPage, deviceList, refreshButton */
+/* global detailPage, batteryState, batteryStateButton, disconnectButton */
+/* global ble  */
+/* jshint browser: true , devel: true*/
+'use strict';
+
+var battery = {
+    service: "180F",
+    level: "2A19"
+};
+
 var app = {
-initialize: function() {
-this.bindEvents();
-this.showMainPage();
-},
-bindEvents: function() {
-var TOUCH_START = 'touchstart';
-if (window.navigator.msPointerEnabled) { // windows phone
-TOUCH_START = 'MSPointerDown';
-}
-document.addEventListener('deviceready', this.onDeviceReady, false);
-refreshButton.addEventListener(TOUCH_START, this.refreshDeviceList, false);
-sendButton.addEventListener(TOUCH_START, this.sendData, false);
-disconnectButton.addEventListener(TOUCH_START, this.disconnect, false);
-deviceList.addEventListener(TOUCH_START, this.connect, false);
-},
-onDeviceReady: function() {
-//This function will be used for switching on the device bluetooth
-cordova.plugins.BluetoothStatus.promptForBT();
-window.bluetooth = cordova.require("cordova/plugin/bluetooth");
-function yourCallbackFunction(){
-window.bluetooth = cordova.require("cordova/plugin/bluetooth");
-}
-bluetoothSerial.isEnabled(success, failure);
-alert('device is ready');
-app.refreshDeviceList();
-},
-refreshDeviceList: function() {
-alert('Get the Device List');
-bluetoothSerial.list(app.onDeviceList, app.onError);
-},
-onDeviceList: function(devices) {
-var option;
-// remove existing devices
-deviceList.innerHTML = "";
-app.setStatus("");
-devices.forEach(function(device) {
-var listItem = document.createElement('li'),
-html = '<b>' + device.name + '</b><br/>' + device.id;
-listItem.innerHTML = html;
-if (cordova.platformId === 'windowsphone') {
-// This is a temporary hack until I get the list tap working
-var button = document.createElement('button');
-button.innerHTML = "Connect";
-button.addEventListener('click', app.connect, false);
-button.dataset = {};
-button.dataset.deviceId = device.id;
-listItem.appendChild(button);
-} else {
-listItem.dataset.deviceId = device.id;
-}
-deviceList.appendChild(listItem);
-});
-if (devices.length === 0) {
-option = document.createElement('option');
-option.innerHTML = "No Bluetooth Devices";
-deviceList.appendChild(option);
-if (cordova.platformId === "ios") { // BLE
-app.setStatus("No Bluetooth Peripherals Discovered.");
-} else { // Android or Windows Phone
-app.setStatus("Please Pair a Bluetooth Device.");
-cordova.plugins.BluetoothStatus.promptForBT();
-}
-} else {
-app.setStatus("Found " + devices.length + " device" + (devices.length === 1 ? "." : "s."));
-}
-},
-showMainPage: function() {
-mainPage.style.display = "";
-detailPage.style.display = "none";
-},
-showDetailPage: function() {
-mainPage.style.display = "none";
-detailPage.style.display = "";
-},
-setStatus: function(message) {
-console.log(message);
-window.clearTimeout(app.statusTimeout);
-statusDiv.innerHTML = message;
-statusDiv.className = 'fadein';
-// automatically clear the status with a timer
-app.statusTimeout = setTimeout(function () {
-statusDiv.className = 'fadeout';
-}, 5000);
-},
-onError: function(reason) {
-alert("ERROR: " + reason); // real apps should use notification.alert
-}
+    initialize: function() {
+        this.bindEvents();
+        detailPage.hidden = true;
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        refreshButton.addEventListener('touchstart', this.refreshDeviceList, false);
+        batteryStateButton.addEventListener('touchstart', this.readBatteryState, false);
+        disconnectButton.addEventListener('touchstart', this.disconnect, false);
+        deviceList.addEventListener('touchstart', this.connect, false); // assume not scrolling
+    },
+    onDeviceReady: function() {
+        app.refreshDeviceList();
+    },
+    refreshDeviceList: function() {
+        deviceList.innerHTML = ''; // empties the list
+        // scan for all devices
+        ble.scan([], 5, app.onDiscoverDevice, app.onError);
+    },
+    onDiscoverDevice: function(device) {
+
+        console.log(JSON.stringify(device));
+        var listItem = document.createElement('li'),
+            html = '<b>' + device.name + '</b><br/>' +
+                'RSSI: ' + device.rssi + '&nbsp;|&nbsp;' +
+                device.id;
+
+        listItem.dataset.deviceId = device.id;  // TODO
+        listItem.innerHTML = html;
+        deviceList.appendChild(listItem);
+
+    },
+    connect: function(e) {
+        var deviceId = e.target.dataset.deviceId,
+            onConnect = function() {
+
+                // TODO check if we have the battery service
+                // TODO check if the battery service can notify us
+                //ble.startNotification(deviceId, battery.service, battery.level, app.onBatteryLevelChange, app.onError);
+                batteryStateButton.dataset.deviceId = deviceId;
+                disconnectButton.dataset.deviceId = deviceId;
+                app.showDetailPage();
+            };
+
+        ble.connect(deviceId, onConnect, app.onError);
+    },
+    onBatteryLevelChange: function(data) {
+        console.log(data);
+        var message;
+        var a = new Uint8Array(data);
+        batteryState.innerHTML = a[0];
+    },
+    readBatteryState: function(event) {
+        console.log("readBatteryState");
+        var deviceId = event.target.dataset.deviceId;
+        ble.read(deviceId, battery.service, battery.level, app.onReadBatteryLevel, app.onError);
+    },
+    onReadBatteryLevel: function(data) {
+        console.log(data);
+        var message;
+        var a = new Uint8Array(data);
+        batteryState.innerHTML = a[0];
+    },
+    disconnect: function(event) {
+        var deviceId = event.target.dataset.deviceId;
+        ble.disconnect(deviceId, app.showMainPage, app.onError);
+    },
+    showMainPage: function() {
+        mainPage.hidden = false;
+        detailPage.hidden = true;
+    },
+    showDetailPage: function() {
+        mainPage.hidden = true;
+        detailPage.hidden = false;
+    },
+    onError: function(reason) {
+        alert("ERROR: " + reason); // real apps should use notification.alert
+    }
 };
